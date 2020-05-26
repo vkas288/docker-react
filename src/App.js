b4130421d2f88b0e55ac151e793e4ba5b09d4705
Singh, Vikas <vikas.singh@hidglobal.com>
2020-05-26 13:06:34 +0530

    updated dockerfile for experimental changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          How you doing...!
+          This is running from AWS Elastic Beanstalk.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
